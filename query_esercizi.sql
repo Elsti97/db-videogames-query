0ef6701c2f2204cf1418d6b36254677b87f21cf5
Elsti97 <elsti.shehati@hotmail.it>
2023-03-29 15:15:50 +0200

    SELECT Done

---
+++ query_esercizi.sql
@@ -0,0 +1,51 @@
+﻿-- SELECT
+
+-- 1
+--SELECT *
+--FROM software_houses
+--WHERE country = 'United States';
+
+-- 2
+--SELECT *
+--FROM players
+--WHERE city = 'Rogahnland';
+
+-- 3
+--SELECT *
+--FROM players
+--WHERE name LIKE '%a';
+
+-- 4
+--SELECT *
+--FROM reviews
+--WHERE player_id = 800;
+
+-- 5
+--SELECT *
+--FROM tournaments
+--WHERE year = 2015;
+
+-- 6
+--SELECT *
+--FROM awards
+--WHERE description LIKE '%facere%';
+
+-- 7
+--SELECT DISTINCT videogame_id
+--FROM category_videogame
+--WHERE category_id = 2 or category_id = 6;
+
+-- 8
+--SELECT *
+--FROM reviews
+--WHERE rating BETWEEN 2 AND 4;
+
+-- 9
+--SELECT *
+--FROM videogames
+--WHERE release_date LIKE '2020%';
+
+-- 10
+--SELECT DISTINCT videogame_id, rating
+--FROM reviews
+--WHERE rating = 5;
